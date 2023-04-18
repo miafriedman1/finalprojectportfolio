5701b9df0ac46e22443391c953db525a2af6f298
miafriedman1 <114001917+miafriedman1@users.noreply.github.com>
2023-04-18 13:16:18 -0400

    Add files via upload

--- js/contact.js
+++ js/contact.js
@@ -8,3 +8,11 @@
             alert('Ok, lets do this!');
         }
     });
+
+let img_alt = document.querySelectorAll("img");
+
+for (let x = 0; x < img_alt.length; x++) {
+    y = img_alt[x];
+    y.addEventListener("click",function(){
+        console.log(this.alt)});
+}
